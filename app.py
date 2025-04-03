b3a64388ab61cab23a3862680c48d60fd4192ca6
Eyli333 <elisee.brand@gmail.com>
2025-04-03 16:10:54 +0200

    adding the host ip

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def delete_machine(machine_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 # {
 #     "name": "LatteLuxe",
